86db7238058cd832061c282a76538e2ef65ba471
unknown <mabbasidesign@yahoo.com>
2018-12-23 12:44:47 +0330

    add linke to sing up

--- tmnt/src/components/layout/SignedInLinks.js
+++ tmnt/src/components/layout/SignedInLinks.js
@@ -1,16 +1,15 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 
-const SignedInLinks = () => {
+const SignedOutLinks = () => {
   return (
     <div>
       <ul className="right">
-        <li><NavLink to='/'>New Project</NavLink></li>
-        <li><NavLink to='/'>Log Out</NavLink></li>
-        <li><NavLink to='/' className="btn btn-floating pink lighten-1">NN</NavLink></li>
+        <li><NavLink to='/signup'>Signup</NavLink></li>
+        <li><NavLink to='/signin'>Login</NavLink></li>
       </ul>
     </div>
   )
 }
 
-export default SignedInLinks
+export default SignedOutLinks
