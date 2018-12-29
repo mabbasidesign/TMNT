1f9713dfd7626e0727692202f515895189df476e
unknown <mabbasidesign@yahoo.com>
2018-12-30 00:12:39 +0330

    sync data with firebase

--- tmnt/src/index.js
+++ tmnt/src/index.js
@@ -1,3 +1,32 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import * as serviceWorker from './serviceWorker';
+// import { createStore, applyMiddleware, compose } from 'redux';
+// import { Provider } from 'react-redux';
+// import rooteReducer from './store/reducers/rooteReducer';
+// import thunk from 'redux-thunk';
+// import { reduxFirestore, getFirestore } from 'redux-firestore';
+// import { reactReduxFirebase, getFirebase } from 'react-redux-firebase';
+// import fbConfig from './config/fbConfig';
+
+// const store = createStore(rooteReducer,
+//     compose(
+//         applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
+//         reduxFirestore(fbConfig),
+//         reactReduxFirebase(fbConfig)
+//         )
+//     );
+
+// ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+
+// serviceWorker.unregister();
+
+
+
+
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -5,20 +34,20 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
-import rooteReducer from './store/reducers/rooteReducer';
+import rootReducer from './store/reducers/rooteReducer';
 import thunk from 'redux-thunk';
 import { reduxFirestore, getFirestore } from 'redux-firestore';
-import { reactReduxFirebase, getFirebase } from 'react-redux-firebase'
+import { reactReduxFirebase, getFirebase } from 'react-redux-firebase';
 import fbConfig from './config/fbConfig';
 
-const store = createStore(rooteReducer,
+const store = createStore(rootReducer,
     compose(
-        applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
-        reduxFirestore(fbConfig),
-        reactReduxFirebase(fbConfig)
-        )
-    );
-
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+      applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore})),
+      reactReduxFirebase(fbConfig), // redux binding for firebase
+      reduxFirestore(fbConfig) // redux bindings for firestore
+    )
+  );
+  
+  ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
 serviceWorker.unregister();
--- tmnt/src/store/reducers/rooteReducer.jsx
+++ tmnt/src/store/reducers/rooteReducer.jsx
@@ -1,10 +1,14 @@
 import authReducer from './authReducer';
 import projectReducer from './projectReducer';
 import { combineReducers } from 'redux';
+import { firestoreReducer } from 'redux-firestore';
 
 const rooteReducer = combineReducers({
     auth: authReducer,
-    project: projectReducer
+    project: projectReducer,
+    firestore: firestoreReducer
 });
 
 export default rooteReducer;
+
+
