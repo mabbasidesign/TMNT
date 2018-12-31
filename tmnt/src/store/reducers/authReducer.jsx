4b7f1205c42c6f2c9cb6ac9008e0be6ca8644cb1
unknown <mabbasidesign@yahoo.com>
2018-12-31 23:40:40 +0330

    sing up user to firebase

--- tmnt/src/store/reducers/authReducer.jsx
+++ tmnt/src/store/reducers/authReducer.jsx
@@ -4,20 +4,38 @@ const initState = {
 
 const authreducer = (state = initState, action) => {
     switch (action.type){
+
         case 'LOGIN_ERROR':
             console.log('login-error')
             return {
                 ...state,
                 authError: 'Lgin failed'
             }
+
         case 'LOGIN_SUCCESS':
             console.log('login failed')
             return {
                 authError: null
             }
+            
         case 'SIGN_OUT_SUCCED':
             console.log('Logout')
             return state;
+
+        case 'SIGNUP_SUCCESS':
+            console.log('signup success')
+            return {
+                ...state,
+                authError: null
+            }
+
+        case 'SIGNUP_ERROR':
+            console.log('signup error')
+            return {
+                ...state,
+                authError: action.err.message
+            }
+
         default:
             return state;
     }    
